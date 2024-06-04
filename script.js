1bbd3550de7788da7e80c4a97e2108341a73458b
Bradley Eversley <16165703+BradleyEversley@users.noreply.github.com>
2024-06-03 22:30:30 -1000

    added new codebase with slider feature to change grid-size

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const board = document.querySelector('.board');
+ // Default grid dimension values
+const gridSize = 400;
+let cellNumber = 16;
 
-    for (let i = 0; i < 16 * 16; i++) {
-        const cell = document.createElement('div');
+// Set grid dimensions
+const board = document.querySelector("#board");
+const sliderContainer = document.querySelector("slider-container");
+const slider = document.querySelector("#slider");
+const sliderValue = document.querySelector("#slider-value");
+
+sliderValue.textContent = `${slider.value} x ${slider.value}`;
+board.style.width = board.style.height = `${gridSize}px`;
+
+ function setBgColor () {
+    this.style.backgroundColor = 'blue';
+ }
+
+ function createGrid(cellNumber) {
+    for (let i = 0; i < (cellNumber * cellNumber); i++) {
+        const cell = document.createElement("div");
+
+        cell.style.width = cell.style.height = `${(gridSize / cellNumber)}px`;
+        cell.classList.add("cell");
         board.appendChild(cell);
+
+        cell.addEventListener("mouseover" , setBgColor);
     }
-});
+ }
+
+ function removeCells() {
+    while (board.firstChild) {
+        board.removeChild(board.firstChild);
+    }
+ }    
+
+//  event listener for changes in slider value
+slider.oninput  = function() {
+    let txt = `${this.value} x ${this.value}`; //captures current value of slider
+    sliderValue.innerHTML = txt; //display grid size
+    removeCells(); //clears current grid
+    createGrid(this.value); //creates new grid with updated value
+    }
+ 
+
+ createGrid(16);
